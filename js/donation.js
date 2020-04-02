505f6341f43c8f547b09bbae5c3c69ba172d8e5a
Nikiforos88 <56295167+Nikiforos88@users.noreply.github.com>
2020-04-02 14:18:15 +0300

    Add files via upload

--- js/donation.js
+++ js/donation.js
@@ -1,3 +1,10 @@
 $(function(){
     $('.selectpicker').selectpicker();
 });
+
+$('.navbar-toggler-icon').click(function(){  
+    if ($('#navbarMenu').hasClass('show')) {
+        $('#navbarMenu').removeClass('show');
+        return false;
+    }
+  });
